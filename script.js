820efb4abcd27b688948dd712f093e52766f86e1
Serhii Prykhozhyi <sprihozhiy@gmail.com>
2016-02-03 16:44:29 +0200

    add html and script

---
+++ script.js
@@ -0,0 +1,27 @@
+var listArr = [];
+
+function addItem() {
+  document.getElementById('infoMsg').innerHTML = '';
+  var node = document.createElement('li');
+  var text = document.getElementById('itemInput').value;
+  var textNode = document.createTextNode(text);
+
+  listArr.push(text);
+  console.log(listArr);
+
+  node.appendChild(textNode);
+  document.getElementById('listBody').appendChild(node);
+  document.getElementById('itemInput').value = '';
+}
+
+function removeItem() {
+  if(listArr.length > 1) {
+    var i = listArr.length;
+    var list = document.getElementById('listBody');
+    list.removeChild(list.childNodes[i]);
+    listArr.pop();
+    console.log(listArr);
+  } else {
+    document.getElementById('infoMsg').innerHTML = "<p>Couldn't be less than 1 task!</p>";
+  }
+}
